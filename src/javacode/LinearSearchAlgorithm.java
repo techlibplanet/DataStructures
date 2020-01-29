25dbf3cf906d5362424638553283a3a65adb8581
techlibplanet <techlibplanet@gmail.com>
2020-01-29 23:40:07 +0530

    Linear search algorithm complete.

---
+++ src/javacode/LinearSearchAlgorithm.java
@@ -0,0 +1,38 @@
+package javacode;
+
+import java.util.Scanner;
+
+/*
+*****Linear search or Sequential Algorithm*****
+* Linear Search - Linear search is used to search a key element from multiple elements.
+* Linear search is less used today because it is slower than binary search and hashing.
+* Complexity : The time complexity of the below algorithm is O(n).
+*/
+
+public class LinearSearchAlgorithm {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{15, 5, 20, 35, 2, 42, 67, 17};
+        println("Enter element to find in array...");
+        Scanner in = new Scanner(System.in);
+        int findElement = in.nextInt();
+        int result = linearSearchAlgorithm(array, findElement);
+        if (result != -1) println("Element is at " + result + " position.");
+        else println("Element not found.");
+    }
+
+    private static int linearSearchAlgorithm(int[] array, int find) {
+        int temp = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == find) {
+                temp = i;
+                break;
+            }
+        }
+        return temp;
+    }
+
+    private static void println(String message) {
+        System.out.println(message);
+    }
+}
