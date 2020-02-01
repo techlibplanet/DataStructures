bbbb3d8430e217e17bf0faf8c6140d0f413a538a
techlibplanet <techlibplanet@gmail.com>
2020-02-01 08:50:58 +0530

    SelectionSort algorithm complete.

--- src/javacode/sorting/SelectionSort.java
+++ src/javacode/sorting/SelectionSort.java
@@ -53,7 +53,7 @@ public class SelectionSort {
         int temp, min;
         for (int i = 0; i < array.length-1; i++) {
             min=i;
-            for (int j = 1; j < array.length; j++) {
+            for (int j = i+1; j < array.length; j++) {
                 if (array[j] < array[min]) {
                     min = j;
                 }
