fce259a563ce92230b63b1cbf8cf0d8db36fe5a8
mayank-wingstop <msharma@wingstop.com>
2021-10-10 00:40:20 +0530

    Added find duplicate elements in array.

---
+++ src/javacode/problems/CheckIfASubarrayWith0SumExistsOrNot.kt
@@ -0,0 +1,37 @@
+package javacode.problems
+
+class CheckIfASubarrayWith0SumExistsOrNot {
+
+    fun hasZeroSumSubArray(array: IntArray): Boolean {
+        val set = hashSetOf<Int>()
+        var sum = 0
+        set.add(sum)
+        for (i in array) {
+            sum +=i
+
+            if (set.contains(sum)){
+                return true
+            }
+
+            set.add(sum)
+        }
+        return false
+    }
+
+    fun printMessage(message: String) {
+        print(message)
+    }
+}
+
+fun main(args: Array<String>) {
+    val array = intArrayOf(3, 4, -7, 3, 1, 3, 1, -4, -2, -2)
+
+    val check = CheckIfASubarrayWith0SumExistsOrNot()
+
+   if (check.hasZeroSumSubArray(array)){
+       check.printMessage("SubArray exist")
+   }else {
+       check.printMessage("SubArray does not exist")
+   }
+
+}
---
+++ src/javacode/problems/FindTheDuplicateElementInALimitedRangeArray.kt
@@ -0,0 +1,37 @@
+package javacode.problems
+
+class FindTheDuplicateElementInALimitedRangeArray {
+
+    fun findDuplicateElement(array: IntArray): HashMap<Int, Int> {
+        val hashMap = hashMapOf<Int, Int>()
+
+        for (e in array) {
+            if (hashMap.containsKey(e)) {
+                var count = hashMap.getValue(e)
+                hashMap[e] = count + 1
+            } else {
+                hashMap[e] = 1
+            }
+        }
+        return hashMap
+    }
+
+    fun traverseHashMap(hashMap: HashMap<Int, Int>) {
+        hashMap.forEach { (key, value) ->
+            if (value != 1) {
+                println("$key is the duplicate element and it appears $value times.")
+            }
+        }
+    }
+}
+
+fun main() {
+    //val array = intArrayOf(1, 2, 3, 4, 4)
+
+    val array = intArrayOf(1, 2, 3, 4, 2, 3, 3)
+
+    val duplicateElement = FindTheDuplicateElementInALimitedRangeArray()
+
+    duplicateElement.traverseHashMap(duplicateElement.findDuplicateElement(array))
+
+}
