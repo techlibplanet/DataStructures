d9203e08e0fc417f4d5919fbb869f82dd2b7ef59
mayank-wingstop <msharma@wingstop.com>
2021-10-11 00:23:08 +0530

    Remove duplicate from array - dojme

--- src/javacode/problems/arrays/RemoveDuplicateElementsFromAnArray.kt
+++ src/javacode/problems/arrays/RemoveDuplicateElementsFromAnArray.kt
@@ -1,4 +1,28 @@
 package javacode.problems.arrays
 
 class RemoveDuplicateElementsFromAnArray {
+    companion object {
+
+        fun removeDuplicateElementFromArray(array: Array<Int>): IntArray {
+            val size = array.size
+            val tempArray = mutableListOf<Int>()
+            for (i in 0 until size) {
+                if (!tempArray.contains(array[i])){
+                    tempArray.add(array[i])
+                }
+            }
+
+            return tempArray.toIntArray()
+        }
+    }
+}
+
+fun main() {
+    val array = arrayOf(1, 1, 2, 2, 3, 4,4, 5, 5)
+
+    val resultArray = RemoveDuplicateElementsFromAnArray.removeDuplicateElementFromArray(array)
+
+    resultArray.forEach {
+        print("$it, ")
+    }
 }
