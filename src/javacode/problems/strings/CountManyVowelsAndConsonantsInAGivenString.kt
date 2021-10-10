bca2be91374f6d5ee0144cbb811895589a1be146
mayank-wingstop <msharma@wingstop.com>
2021-10-10 13:54:06 +0530

    Count number of Vowels in string - done

--- src/javacode/problems/strings/CountManyVowelsAndConsonantsInAGivenString.kt
+++ src/javacode/problems/strings/CountManyVowelsAndConsonantsInAGivenString.kt
@@ -1,4 +1,29 @@
 package javacode.problems.strings
 
 class CountManyVowelsAndConsonantsInAGivenString {
+
+    companion object{
+        fun countVowels(input: String): HashMap<Char, Int> {
+            val charArray = input.toCharArray()
+            val vowels = charArrayOf('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+            val hashMap = hashMapOf<Char, Int>()
+
+            for (c in charArray) {
+                if (vowels.contains(c)) {
+                    if (hashMap.containsKey(c)) {
+                        val count = hashMap.getValue(c)
+                        hashMap[c] = count + 1
+                    } else hashMap[c] = 1
+                }
+            }
+
+            return hashMap
+        }
+    }
+}
+
+fun main() {
+    val input = "I Love You"
+
+    print(CountManyVowelsAndConsonantsInAGivenString.countVowels(input))
 }
