354ea8945aed7861ceb3ffee9cf5a08de4bf5476
mayank-wingstop <msharma@wingstop.com>
2021-10-10 13:41:19 +0530

    Reverse string using loop and recursion

--- src/javacode/problems/strings/ReverseAGivenString.kt
+++ src/javacode/problems/strings/ReverseAGivenString.kt
@@ -1,4 +1,32 @@
 package javacode.problems.strings
 
+import java.lang.StringBuilder
+
 class ReverseAGivenString {
+    companion object{
+        fun reverseString(input : String): String {
+            val charArray = input.toCharArray()
+            val stringBuilder = StringBuilder()
+
+            for (i in charArray.indices.reversed()){
+                stringBuilder.append(charArray[i])
+            }
+
+            return stringBuilder.toString()
+        }
+
+        fun reverseStringUsingRecursion(input: String): String {
+            if (input.length < 2) {
+                return input;
+            }
+
+            return reverseStringUsingRecursion(input.substring(1)) + input[0]
+        }
+    }
+}
+
+fun main() {
+    val input = "Mayank"
+
+    print(ReverseAGivenString.reverseStringUsingRecursion(input))
 }
