e8c5f04424b7fd9f4d61a12ab44f0431d47dc10c
mayank-wingstop <msharma@wingstop.com>
2021-10-12 13:42:53 +0530

    Add-Subtract-Multiply 2 matrices done

--- src/javacode/problems/matrices/AddOrSubtractTwoMatrices.kt
+++ src/javacode/problems/matrices/AddOrSubtractTwoMatrices.kt
@@ -1,4 +1,47 @@
 package javacode.problems.matrices
 
 class AddOrSubtractTwoMatrices {
+    companion object{
+
+        fun findSumOfMatrices(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
+            val resultArray = arrayOf(IntArray(arr1.size), IntArray(arr2.size))
+
+            for (i in arr1.indices) for (j in arr2.indices) resultArray[i][j] = arr1[i][j] + arr2[i][j]
+
+            return resultArray
+        }
+
+        fun findSubtractionOfMatrices(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
+            val resultArray = arrayOf(IntArray(arr1.size), IntArray(arr2.size))
+
+            for (i in arr1.indices) for (j in arr2.indices){
+                resultArray[i][j] = arr1[i][j] - arr2[i][j]
+            }
+
+            return resultArray
+        }
+    }
+}
+
+fun main() {
+    val arr1 = arrayOf(intArrayOf(1, 20), intArrayOf(13, 14))
+    val arr2 = arrayOf(intArrayOf(3, 4), intArrayOf(5, 6))
+    val addition = AddOrSubtractTwoMatrices.findSumOfMatrices(arr1, arr2)
+    val subtraction = AddOrSubtractTwoMatrices.findSubtractionOfMatrices(arr1, arr2)
+
+    print("Addition of matrices\n")
+    for (i in addition){
+        for(j in i){
+            print("$j ")
+        }
+    }
+
+
+    print("Subtraction of matrices\n")
+
+    for (i in subtraction){
+        for(j in i){
+            print("$j ")
+        }
+    }
 }
--- src/javacode/problems/matrices/MultiplyTwoMatrices.kt
+++ src/javacode/problems/matrices/MultiplyTwoMatrices.kt
@@ -1,4 +1,25 @@
 package javacode.problems.matrices
 
 class MultiplyTwoMatrices {
+    companion object{
+        fun multiplyTwoMatrices(array1: Array<IntArray>, array2 : Array<IntArray>): Array<IntArray> {
+            val resultArray = arrayOf(IntArray(array1.size), IntArray(array2.size))
+
+            for (i in array1.indices) for (j in array2.indices){
+                resultArray[i][j] = array1[i][j] * array2[i][j]
+            }
+
+            return resultArray
+        }
+    }
+}
+
+fun main() {
+    val arr1 = arrayOf(intArrayOf(1, 20), intArrayOf(13, 14))
+    val arr2 = arrayOf(intArrayOf(3, 4), intArrayOf(5, 6))
+    val multiply = MultiplyTwoMatrices.multiplyTwoMatrices(arr1, arr2)
+
+    for (i in multiply) for (j in i){
+        print("$j ")
+    }
 }
